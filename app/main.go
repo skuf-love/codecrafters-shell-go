3e93980033664043d50d9e09f1e788d1ab0e2771
Kostya Malinovskiy <kostya.malinovskiy@gmail.com>
2025-12-04 22:10:51 +0200

    Loop prompt

--- app/main.go
+++ app/main.go
@@ -10,11 +10,13 @@ import (
 var _ = fmt.Print
 
 func main() {
-	fmt.Print("$ ")
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Println(os.Stderr, "Error reading input:", err)
-		os.Exit(1)
+	for {
+		fmt.Print("$ ")
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Println(os.Stderr, "Error reading input:", err)
+			os.Exit(1)
+		}
+		fmt.Println(command[:len(command)-1] + ": command not found")
 	}
-	fmt.Println(command[:len(command)-1] + ": command not found")
 }
