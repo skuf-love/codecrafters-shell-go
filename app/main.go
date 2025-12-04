2488390aa5db5228a272cba4a66cae6a6c3f324e
Kostya Malinovskiy <kostya.malinovskiy@gmail.com>
2025-12-04 21:57:33 +0200

    Accept command and tell it is invalid

--- app/main.go
+++ app/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"bufio"
+	"os"
 )
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
@@ -9,4 +11,6 @@ var _ = fmt.Print
 
 func main() {
 	fmt.Print("$ ")
+	command, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	fmt.Println(command[:len(command)-1], " - invalid command")
 }
