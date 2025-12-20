ae96f7de008604194c3684ab3bef72bb62ff437f
Kostya Malinovskiy <kostya.malinovskiy@gmail.com>
2025-12-20 22:21:11 +0200

    Update readUntilPrompt function to read output by byte in goroutine. Use timeout and echo special stop symbol to gracefuly finish the goroutine

--- app/main_test.go
+++ app/main_test.go
@@ -8,6 +8,7 @@ import(
 	"fmt"
 	"io"
 	"bufio"
+	"time"
 )
 
 
@@ -55,22 +56,56 @@ func eatInitPrompt(reader *bufio.Reader, t *testing.T) {
  		t.Fatal(err)
 	}
 }
-func readUntilPrompt(reader *bufio.Reader, t *testing.T) (string, error) {
-	
-	
-	line, err := reader.ReadString('\n')
-	if err == io.EOF {
-		return "", err
-	}
+func (c ShellTestContext) readUntilPrompt(reader *bufio.Reader, t *testing.T) (string, error) {
+	var result strings.Builder
 
-	line, _ = strings.CutSuffix(line, "\n$")
-	return line, nil
+	buf := make(chan byte, 1)
+	done := make(chan struct{})
+	c.log("About to start goroutine")
+	go func(){
+		c.log("Goroutine started")
+		var b byte
+		var err error
+		defer close(done)
+		for {
+			b, _ = reader.ReadByte()
+			c.log(fmt.Sprintf("Inside goroutine reading byte: %q(%v)", b, b))
+			if err != nil {
+				c.log(fmt.Sprintf("Inside goroutine error: %v", err))
+				t.Fatal(err)
+				close(buf)
+				break
+			}
+			if b == byte('%') {
+				c.log("Inside goroutine closing the channel")
+				reader.ReadByte() // read \n after %
+				close(buf)
+				break
+			}else{
+				buf <- b
+			}
+		}
+	}()
+		
+	for {
+		select {
+		case anotherByte := <- buf:
+			result.WriteByte(anotherByte)
+		case <- time.After(200 * time.Millisecond):
+			c.log(fmt.Sprintf("Timeout goroutine, received result: %v", result.String()))
+			c.stdin.Write([]byte("echo %\n")) // use % symbol to signal goroutine to stop reading and finis
+			<- done
+			c.log(fmt.Sprintf("Done received: %v", result.String()))
+			return result.String(), nil
+		}
+	}
 }
 
 func (c ShellTestContext) assertCmd(input string, expectedOutput string) {
 	c.sendInput(input)
+	c.log("  AssertCmd: " + input)
 
-	output, err := readUntilPrompt(c.stdoutReader, c.t)
+	output, err := c.readUntilPrompt(c.stdoutReader, c.t)
 
 	if err != nil {
 	    c.t.Fatalf("Failed to read initial prompt: %v", err)
@@ -90,7 +125,9 @@ func (c ShellTestContext) assertCmd(input string, expectedOutput string) {
 func (c ShellTestContext) tearDown() {
 	c.sendInput("exit\n")
 
+	c.logFile.WriteString("< < <\n")
 	c.stdin.Close()
+	c.logFile.Close()
 
 	stderrBytes, err := io.ReadAll(c.stderr)
 	stdoutBytes, err := io.ReadAll(c.stdout)
@@ -119,16 +156,23 @@ type ShellTestContext struct {
 	stdout io.ReadCloser
 	stderr io.ReadCloser
 	stdoutReader *bufio.Reader
+	logFile *os.File
 }
 func InitTest(t *testing.T) ShellTestContext {
 	cmd := exec.Command("./testapp")
 	stdin, stdout, stderr := SetupPipes(t, cmd)
 	stdoutReader := bufio.NewReader(stdout)
+	logFile, _ := os.OpenFile("./test.log", os.O_APPEND|os.O_WRONLY, 0644)
+	logFile.WriteString("> > > Example Log Started\n")
+//	if err != nil {
+//		t.Fatal(err)
+//	}
 
- 	if err := cmd.Start(); err != nil {
+	if err := cmd.Start(); err != nil {
  		t.Fatal(err)
  	}
 	
+	logFile.WriteString("Command Started\n")
 //	eatInitPrompt(stdoutReader, t)
 
 	return ShellTestContext{
@@ -138,53 +182,58 @@ func InitTest(t *testing.T) ShellTestContext {
 		stdout,
 		stderr,
 		stdoutReader,
+		logFile,
 	}
 }
 
+func (c ShellTestContext) log(entry string){
+	c.logFile.WriteString(entry + "\n")
+}
+
 func TestLocateExecutableFiles(t *testing.T) {
-	
+	//InitTest(t)
+//	context := InitTest(t)
+
+//	context.assertCmd("type echo\n", "echo is a shell builtin")
+//	context.assertCmd("type type\n", "type is a shell builtin")
+//	context.assertCmd("type exit\n", "exit is a shell builtin")
+//
+//	context.assertCmd("type grep\n", "grep is /usr/bin/grep")
+//	context.assertCmd("type invalid_command\n", "invalid_command: not found")
+
+//	context.tearDown()
+}
+
+func TestPwd(t *testing.T) { 
 	context := InitTest(t)
-//
-	context.assertCmd("type echo\n", "echo is a shell builtin")
-	context.assertCmd("type type\n", "type is a shell builtin")
-	context.assertCmd("type exit\n", "exit is a shell builtin")
-//
-	context.assertCmd("type grep\n", "grep is /usr/bin/grep")
-	context.assertCmd("type invalid_command\n", "invalid_command: not found")
-//
+	
+	home_path := os.Getenv("HOME")
+	context.assertCmd("pwd\n", home_path + "/study/go/codecrafters-shell-go/app")
+
 	context.tearDown()
 }
-//
-//func TestPwd(t *testing.T) { 
-//	context := InitTest(t)
-//	
-//	home_path := os.Getenv("HOME")
-//	context.assertCmd("pwd\n", home_path + "/study/go/codecrafters-shell-go/app")
-//
-//	context.tearDown()
-//}
 
 //func TestCd(t *testing.T) { 
 //	context := InitTest(t)
 //	home_path := os.Getenv("HOME")
 //
-//	context.sendInput("cd ~\n")
+//	context.sendInput("cd ~\n\n")
 //	readUntilPrompt(context.stdoutReader, context.t)
 //	context.assertCmd("pwd\n", home_path)
 //
-//	context.sendInput("cd .\n")
+//	context.sendInput("cd .\n\n")
 //	readUntilPrompt(context.stdoutReader, context.t)
 //	context.assertCmd("pwd\n", home_path)
 //
-//	context.sendInput("cd ..\n")
+//	context.sendInput("cd ..\n\n")
 //	readUntilPrompt(context.stdoutReader, context.t)
-//	context.assertCmd("pwd\n", "/Users")
+//	context.assertCmd("pwd\n\n", "/Users")
 //
 //	context.sendInput("cd " + home_path + "/study\n")
 //	readUntilPrompt(context.stdoutReader, context.t)
 //	context.assertCmd("pwd\n", home_path + "/study")
 //
-//	context.sendInput("cd go\n")
+//	context.sendInput("cd go\n\n")
 //	readUntilPrompt(context.stdoutReader, context.t)
 //	context.assertCmd("pwd\n", home_path + "/study/go")
 //
@@ -200,28 +249,28 @@ func TestLocateExecutableFiles(t *testing.T) {
 //
 //	context.tearDown()
 //}
-//
+
 //func TestStdout(t *testing.T) {
 //	context := InitTest(t)
 //
-//	context.sendInput("echo hello 1> file1.txt\n")
+//	context.sendInput("echo hello 1> file1.txt\n\n")
 //	readUntilPrompt(context.stdoutReader, context.t)
 //	context.assertCmd("cat file1.txt\n", "hello")
 //	os.Remove("file1.txt")
 //
-//	context.sendInput("echo hello > file2.txt\n")
+//	context.sendInput("echo hello > file2.txt\n\n")
 //	readUntilPrompt(context.stdoutReader, context.t)
 //	context.assertCmd("cat file2.txt\n", "hello")
 //	os.Remove("file2.txt")
 //
 //
 //
-//	context.sendInput("ls -1 pig > cow/dog.md\n")
-//	readUntilPrompt(context.stdoutReader, context.t)
-//	context.assertCmd("cat cow/dog.md\n", "grape\norange\npear")
-//	os.Remove("cow/dog.md")
+//	//context.sendInput("ls -1 pig > cow/dog.md\n\n")
+//	//readUntilPrompt(context.stdoutReader, context.t)
+//	//context.assertCmd("cat cow/dog.md\n", "grape\norange\npear")
+//	//os.Remove("cow/dog.md")
 //
-//	context.assertCmd("cat pig/grape nonexistent 1> cow/fox.md\n", "cat: nonexistent: No such file or directory")
+//	//context.assertCmd("cat pig/grape nonexistent 1> cow/fox.md\n", "cat: nonexistent: No such file or directory")
 //
 //	context.tearDown()
 //}
